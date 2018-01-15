9c0e77e658444d51c8f37c2362b66312698b589c
Zhongjie He <eric6356@gmail.com>
2018-01-15 16:53:17 -0500

    login

--- assignment-1/imports/startup/client/routes.js
+++ assignment-1/imports/startup/client/routes.js
@@ -1,3 +1,5 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import { ActiveRoute } from 'meteor/zimme:active-route';
@@ -11,26 +13,37 @@ import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
 import '../../ui/pages/account/account.js';
 import '../../ui/pages/access/access.js';
+import '../../ui/pages/portal/portal.js';
 
 // Set up all routes in the app
-FlowRouter.route('/', {
-    name: 'home',
+function makePrivateRouter(path, name) {
+    FlowRouter.route(path, {
+        name,
+        triggersEnter: function(context, redirect) {
+            if (Meteor.user()) {
+                BlazeLayout.render('App_body', { main: name });
+            } else {
+                redirect(`/portal?next=${encodeURIComponent(path)}`);
+            }
+        }
+    });
+}
+
+makePrivateRouter('/', 'home');
+makePrivateRouter('/account', 'account');
+makePrivateRouter('/access', 'access');
+
+FlowRouter.route('/portal', {
+    name: 'portal',
     action() {
-        BlazeLayout.render('App_body', { main: 'home' });
+        BlazeLayout.render('App_body', { main: 'portal' });
     }
 });
 
-FlowRouter.route('/account', {
-    name: 'account',
+FlowRouter.route('/logout', {
+    name: 'logout',
     action() {
-        BlazeLayout.render('App_body', { main: 'account' });
-    }
-});
-
-FlowRouter.route('/access', {
-    name: 'access',
-    action() {
-        BlazeLayout.render('App_body', { main: 'access' });
+        Meteor.logout(() => FlowRouter.go('/portal'));
     }
 });
 
@@ -39,3 +52,8 @@ FlowRouter.route('/access', {
 //         BlazeLayout.render('App_body', { main: 'App_notFound' });
 //     }
 // };
+
+Accounts.onLogin(() => {
+    const next = FlowRouter.current().queryParams.next || '/';
+    FlowRouter.go(next);
+});
--- assignment-1/imports/ui/layouts/body/body.js
+++ assignment-1/imports/ui/layouts/body/body.js
@@ -1,12 +1,1 @@
-import { Template } from 'meteor/templating';
-import { ActiveRoute } from 'meteor/zimme:active-route';
-import { FlowRouter } from 'meteor/kadira:flow-router';
-
 import './body.html';
-
-Template.App_body.helpers({
-    activeListClass(list) {
-        const active = ActiveRoute.name('Lists.show') && FlowRouter.getParam('_id') === list._id;
-        return active && 'active';
-    }
-});
